ff0d2a88d8c12b0e83d96fe02fcd4362c74a654a
Pravesh Narayan <artbypravesh15@gmail.com>
2019-10-21 21:22:08 +1100

    Add header
    > Create header.js

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+  <header>
+    <h1>ArtbyPravesh Project Documentations</h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  </header>
+)
