dae8e107b248bad8a54211dce1eea45f01f77c8e
Pravesh Narayan <artbypravesh15@gmail.com>
2019-10-21 21:35:01 +1100

    Add navigational links
    Using gatsby Link

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 export default props => (
   <header>
-    <h3>ArtbyPravesh Project Documentations</h3>
+    <Link to='//'><h3>ArtbyPravesh Project Documentations</h3></Link>
     <nav>
       <ul>
-        <li>Home</li>
-        <li>About</li>
+        <li><Link to='//'>Home</Link></li>
+        <li><Link to='/about/'>About</Link></li>
       </ul>
     </nav>
     <h1>{props.pageTitle}</h1>
